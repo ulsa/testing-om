ef0c78f2aa5089018aca5b551855c87ecbcc520d
Oskar Wickström <oskar.wickstrom@live.com>
2014-01-04 12:57:03 +0100

    Periodically read comment updates from server.

--- src/testing_om/core.cljs
+++ src/testing_om/core.cljs
@@ -1,7 +1,7 @@
 (ns testing-om.core
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
-            [cljs.core.async :as async :refer [<! put! chan]]
+            [cljs.core.async :as async :refer [<! put! chan alts! timeout]]
             [ajax.core :refer [GET POST]])
   (:require-macros
    [cljs.core.async.macros :refer [go]]))
@@ -35,18 +35,47 @@
 (defn trim-field [f]
   (.. f -value trim))
 
+(defn make-handler [ch] (fn [r] (put! ch r)))
+
+(defn get-json [uri]
+  (let [success (chan)
+        error (chan)]
+    (GET uri
+               {:handler (make-handler success)
+                :error-handler (make-handler error)
+                :response-format :json
+                :keywords? true
+                :timeout 10})
+    {:success success :error error}))
+
 (defn post-json [uri data]
   (let [success (chan)
         error (chan)]
     (POST uri
-               {:handler (fn [response] (put! success response))
-                :error-handler (fn [response] (put! error response))
+               {:handler (make-handler success)
+                :error-handler (make-handler error)
                 :params data
                 :format :json
                 :response-format :json
                 :keywords? true
                 :timeout 10})
     {:success success :error error}))
+
+(def comment-updates
+  (let [out (chan)]
+    (go (while true
+          (<! (timeout 1000))
+          (let [{:keys [success error] :as chs} (get-json "/comments.json")
+                [result ch] (alts! (vals chs))]
+            (if (= ch success)
+              (put! out result)
+              (log (str "Comment updates polling failed: " (get-in result [:response :error])))))))
+    out))
+
+(defn update-comments [app]
+  (go (while true
+        (let [updated (<! comment-updates)] 
+          (om/transact! app :comments (constantly updated))))))
 
 (defn handle-form-submit [e app owner]
   (let [author-field (om/get-node owner "author")
@@ -83,10 +112,11 @@
 
 (defn comment-box [app]
   (om/component
-   (dom/div #js {:className "commentBox"}
-            (dom/h1 nil "Comments")
-            (om/build comment-list app)
-            (om/build comment-form app))))
+    (update-comments app)
+    (dom/div #js {:className "commentBox"}
+             (dom/h1 nil "Comments")
+             (om/build comment-list app)
+             (om/build comment-form app))))
 
 (om/root app-state comment-box (.getElementById js/document "content"))
 
