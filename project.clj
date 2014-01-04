5262b37fe1d2feab252a581a645806fe7b8cc0f3
Oskar Wickström <oskar.wickstrom@live.com>
2014-01-04 10:32:41 +0100

    Use om 0.1.3.

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2138"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [om "0.1.0-SNAPSHOT"]]
+                 [om "0.1.3"]]
 
   :plugins [[lein-cljsbuild "1.0.1"]]
 
