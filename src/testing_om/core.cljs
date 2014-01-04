1a565625077a746bd6870fe7fe8b48d8ced44309
Oskar Wickström <oskar.wickstrom@live.com>
2014-01-04 13:02:14 +0100

    Remove old React code, om code is feature complete.

--- src/testing_om/core.cljs
+++ src/testing_om/core.cljs
@@ -64,7 +64,7 @@
 (def comment-updates
   (let [out (chan)]
     (go (while true
-          (<! (timeout 1000))
+          (<! (timeout 5000))
           (let [{:keys [success error] :as chs} (get-json "/comments.json")
                 [result ch] (alts! (vals chs))]
             (if (= ch success)
@@ -119,74 +119,3 @@
              (om/build comment-form app))))
 
 (om/root app-state comment-box (.getElementById js/document "content"))
-
-(comment
-  "
-var CommentForm = React.createClass({
-  handleSubmit: function() {
-    var author = this.refs.author.getDOMNode().value.trim();
-    var text = this.refs.text.getDOMNode().value.trim();
-    if (!text || !author) {
-      return false;
-    }
-    this.props.onCommentSubmit({author: author, text: text});
-    this.refs.author.getDOMNode().value = '';
-    this.refs.text.getDOMNode().value = '';
-    return false;
-  },
-  render: function() {
-    return (React.DOM.form(
-      {className: "commentForm ", onSubmit: this.handleSubmit},
-      React.DOM.input({type: "text ", placeholder: "Your name ", ref: "author "}),
-      React.DOM.input({type: "text ", placeholder: "Say something... ", ref: "text "}),
-      React.DOM.input({type: "submit ", value: "Post "})
-    ));
-  }
-});
-// tutorial1.js
-var CommentBox = React.createClass({
-  getInitialState: function() {
-    return {data: []};
-  },
-  loadCommentsFromServer: function() {
-    $.ajax({
-      url: 'comments.json',
-      dataType: 'json',
-      success: function(data) {
-        this.setState({data: data});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error("comments.json ", status, err.toString());
-      }.bind(this)
-    });
-  },
-  handleCommentSubmit: function(comment) {
-    var comments = this.state.data;
-    var newComments = comments.concat([comment]);
-    this.setState({data: newComments});
-    $.ajax({
-      url: this.props.url,
-      type: 'POST',
-      data: comment,
-      success: function(data) {
-        this.setState({data: data});
-      }.bind(this)
-    });
-  },
-  componentWillMount: function() {
-    this.loadCommentsFromServer();
-    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
-  },
-  render: function() {
-    return React.DOM.div({className: "commentBox "},
-        React.DOM.h1("Comments "),
-        CommentList({data: this.state.data}),
-        CommentForm({onCommentSubmit: this.handleCommentSubmit})
-    );
-  }
-});
-React.renderComponent(
-  CommentBox({url: "comments.json ", pollInterval: 10000}),
-  document.getElementById('content')
-);
-")
